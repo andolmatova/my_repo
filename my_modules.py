ea5560338a9d2ca9787cb7bb465c7cb545a412f1
Anastasiya Dolmatova <andolmatova@ozon.ru>
2022-01-27 13:17:54 +0300

    Adds comments

--- my_modules.py
+++ my_modules.py
@@ -1,5 +1,9 @@
+# Previously I had some complex logic in this
+# module, but I had to delete it due to reasons.
+# Very sad!
+
 def my_function(first, second):
-    return first+second;
+    return first + second + 10;
 
 def another_function(first):
-    return first+1;
+    return first + 1;
